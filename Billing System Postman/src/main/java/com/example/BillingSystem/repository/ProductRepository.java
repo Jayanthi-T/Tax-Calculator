24484cbccd50620921ea9badf8b8cd32d3c59a94
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2022-04-04 10:05:37 +0530

    Update ProductRepository.java

--- Billing System Postman/src/main/java/com/example/BillingSystem/repository/ProductRepository.java
+++ Billing System Postman/src/main/java/com/example/BillingSystem/repository/ProductRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductList, Long> {
+public interface ProductRepository extends CrudRepository<ProductList, Long> {
 
 }
